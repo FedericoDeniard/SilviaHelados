d1193477bcf0fa0299a0ce4a1dcbd7601ff9156b
Federico Deniard <fededeniard@gmail.com>
2024-01-23 03:52:36 -0300

    WIP Footer mobile

---
+++ src/components/ShopInfo/index.jsx
@@ -0,0 +1,55 @@
+import "./index.css";
+import location from "../../assets/location.svg";
+import time from "../../assets/time.svg";
+import { useState } from "react";
+
+const ShopInfo = () => {
+  const [locationIsPressed, setLocationIsPressed] = useState(false);
+  const [timeIsPressed, setTimeIsPressed] = useState(false);
+
+  const locationPressed = () => {
+    setLocationIsPressed(true);
+  };
+  const timePressed = () => {
+    setTimeIsPressed(true);
+  };
+
+  const clearTimePressed = () => {
+    setTimeIsPressed(false);
+  };
+
+  const clearLocationPressed = () => {
+    setLocationIsPressed(false);
+  };
+
+  return (
+    <ul className="example-1">
+      <li
+        className={`icon-content ${locationIsPressed ? "pressed" : ""}`}
+        onTouchStart={locationPressed}
+        onTouchEnd={clearLocationPressed}
+      >
+        <a className="link" data-social="location" aria-label="location">
+          <img className="location-img" src={location}></img>
+        </a>
+        <div className="tooltip">
+          Gualeguay/CABA
+          <br />
+          Entre Rios 2940
+          <br />
+        </div>
+      </li>
+      <li
+        className={`icon-content ${timeIsPressed ? "pressed" : ""}`}
+        onTouchStart={timePressed}
+        onTouchEnd={clearTimePressed}
+      >
+        <a className="link" data-social="time" aria-label="time">
+          <img className="time-img" src={time}></img>
+        </a>
+        <div className="tooltip">Abierto de 10am a 22pm</div>
+      </li>
+    </ul>
+  );
+};
+export default ShopInfo;
--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -1,12 +1,18 @@
+import ShopInfo from "../ShopInfo";
 import "./index.css";
 
 const Footer = () => {
   return (
     <footer className="footer">
-      <p>Gualeguay/CABA</p>
-      <p>Shopping los altos de Gualeguay</p>
-      <p>Entre Rios 2940</p>
-      <p>Abierto de 10am hasta las 22pm</p>
+      <div className="footer-pc">
+        <p>Gualeguay/CABA</p>
+        <p>Shopping los altos de Gualeguay</p>
+        <p>Entre Rios 2940</p>
+        <p>Abierto de 10am hasta las 22pm</p>
+      </div>
+      <div className="footer-mobile">
+        <ShopInfo />
+      </div>
     </footer>
   );
 };
